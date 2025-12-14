94809d59bf2e6da5f4e07805c2ca9a8ea6f45c65
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-12-14 20:22:34 +0200

    feat: add logging and authentication middleware for enhanced security and request tracking

--- api/toy/toy.route.js
+++ api/toy/toy.route.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import { getToys, getToyById, addToy, updateToy, removeToy } from './toy.controller.js'
+import { requireAuth, requireAdmin } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/logger.middleware.js'
+
 
 const router = express.Router()
 
-router.get('/', getToys)
-router.get('/:id', getToyById)
-router.post('/', addToy)
-router.put('/', updateToy)
-router.delete('/:id', removeToy)
+router.get('/', log, getToys)
+router.get('/:id', log, getToyById)
+
+// Protected routes
+router.post('/', requireAdmin, addToy)
+router.put('/', requireAdmin, updateToy)
+router.delete('/:id', requireAdmin, removeToy)
 
 export const toyRoutes = router
