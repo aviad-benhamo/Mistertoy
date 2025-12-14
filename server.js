3712a78bc796b401d3d7b6c32a966b79e6028c94
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-12-14 15:25:07 +0200

    feat: implement async await syntax

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-
+import { toyService } from './services/toy.service.js'
 
 const app = express()
 
@@ -19,9 +19,70 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 
-// Routes (Example)
-app.get('/api/toy', (req, res) => {
-    res.send('Toy API is ready')
+// Routes
+
+// READ (List)
+app.get('/api/toy', async (req, res) => {
+    try {
+        const filterBy = req.query
+        const toys = await toyService.query(filterBy)
+        res.send(toys)
+    } catch (err) {
+        console.log('Cannot load toys', err)
+        res.status(400).send('Cannot load toys')
+    }
+})
+
+// READ (Get By Id)
+app.get('/api/toy/:toyId', async (req, res) => {
+    try {
+        const { toyId } = req.params
+        const toy = await toyService.getById(toyId)
+        res.send(toy)
+    } catch (err) {
+        console.log('Cannot load toy', err)
+        res.status(400).send('Cannot load toy')
+    }
+})
+
+// CREATE
+app.post('/api/toy', async (req, res) => {
+    try {
+        const { name, price, inStock, labels } = req.body
+        const toyToSave = { name, price, inStock, labels }
+
+        const savedToy = await toyService.save(toyToSave)
+        res.send(savedToy)
+    } catch (err) {
+        console.log('Cannot add toy', err)
+        res.status(400).send('Cannot add toy')
+    }
+})
+
+// UPDATE
+app.put('/api/toy', async (req, res) => {
+    try {
+        const { _id, name, price, inStock, labels } = req.body
+        const toyToSave = { _id, name, price, inStock, labels }
+
+        const savedToy = await toyService.save(toyToSave)
+        res.send(savedToy)
+    } catch (err) {
+        console.log('Cannot update toy', err)
+        res.status(400).send('Cannot update toy')
+    }
+})
+
+// DELETE
+app.delete('/api/toy/:toyId', async (req, res) => {
+    try {
+        const { toyId } = req.params
+        await toyService.remove(toyId)
+        res.send({ msg: 'Removed successfully' })
+    } catch (err) {
+        console.log('Cannot remove toy', err)
+        res.status(400).send('Cannot remove toy')
+    }
 })
 
 
