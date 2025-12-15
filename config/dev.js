78f6c4a6ed7e8b7547422888a6d2af020cb9a74a
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-12-15 21:35:34 +0200

    feat: configure dotenv for environment variable management in development and production

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
     dbName: 'MisterToyDB',
 }
