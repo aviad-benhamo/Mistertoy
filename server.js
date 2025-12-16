bf7a97d38c1a8b36e26c8808353edeec7603953b
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-12-16 20:04:43 +0200

    Add initial HTML structure, CSS styles, and SVG icon for the project
    - Created index.html with basic structure and linked CSS and JS files.
    - Added index-svsUDCQ-.css for styling the application with custom variables and responsive design.
    - Included vite.svg as the favicon for the application.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
@@ -14,11 +15,15 @@ app.use(cookieParser())
 app.use(express.json())
 
 
-const corsOptions = {
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
-    credentials: true
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
 
 
 // Routes
